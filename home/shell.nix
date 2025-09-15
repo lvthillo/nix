78a1a7e1e1dc200c7c3770f09be127cf03d3dd0b
Lorenz Vanthillo <lorenz.vanthillo@gmail.com>
2025-09-15 20:45:02 +0200

    chore: all alias and fix zscaler again

--- home/shell.nix
+++ home/shell.nix
@@ -16,6 +16,7 @@
       tfapply = "terraform apply";
       tfplan = "terraform plan";
       czm = "cz commit";
+      gco = "git checkout";
       devex-prod = "export AWS_PROFILE=devex-prod-admin";
       devex-non-prod = "export AWS_PROFILE=devex-non-prod-admin";
       ll = "ls -l";
@@ -27,6 +28,7 @@
     GOPATH = "$HOME/go";
     AWS_CA_BUNDLE = "/opt/homebrew/etc/ca-certificates/cert.pem";
     JAVA_HOME = "/opt/homebrew/Cellar/openjdk/24.0.2/libexec/openjdk.jdk/Contents/Home/";
+    NODE_EXTRA_CA_CERTS = "$HOME/.zcli/zscaler_root.pem";
   };
 
   home.sessionPath = [
