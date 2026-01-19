d18d4ece9fa7c59a698777efd375dc6b85430ca1
Lorenz Vanthillo <lorenz.vanthillo@gmail.com>
2026-01-19 10:27:51 +0100

    feat: increase maxRequests for chat agent in copilot

--- home/vscode.nix
+++ home/vscode.nix
@@ -84,7 +84,7 @@
           "scminput" = false;
         };
         "github.copilot.nextEditSuggestions.enabled" = true;
-        "chat.agent.maxRequests" = 100;
+        "chat.agent.maxRequests" = 500;
         "chat.customAgentInSubagent.enabled" = true;
         "chat.todoListTool.writeOnly" = true;
         "chat.mcp.gallery.enabled" = true;
